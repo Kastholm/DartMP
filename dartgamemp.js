d6b1bdc265c5bd2169c2bc43b80f46d496fdf146
Kastholm <Kastholm1995@gmail.com>
2022-10-13 20:04:52 +0200

    hsSpil update

--- dartgamemp.js
+++ dartgamemp.js
@@ -356,26 +356,44 @@ document.querySelector(".restart").addEventListener("click", () => {
     });
 });
 
+/* -------------------------------------------------------------------------- */
+/*                                HighscoreSpil                               */
+/* -------------------------------------------------------------------------- */
+/* ------------------------- Fremviser HighscoreSpil ------------------------ */
+document.querySelector(".highScore").addEventListener("click", () => {
+  document.querySelector(".board").classList.add("boardShow");
+});
+/* --------------------- Highscorespil informationsboks --------------------- */
 const highscorePlayers = Swal.mixin({
   customClass: {
     confirmButton: "btn btn-success",
     cancelButton: "btn btn-danger",
   },
   buttonsStyling: false,
-}); /* background-color: #1aa864;
- background-color: #992b2e; */
+});
 highscorePlayers
   .fire({
     title: `<h1 style='font-size: 2.7rem;'>Velkommen til <br style='margin: 0em'> <b style='font-size: 3.5rem; '>HighScore Spil</b><h1><br><h5>Vælg holdene der skal spille mod hinanden</h5><div style='background-color: #992b2e; padding: .2em 0' margin: .2em 0><p style='font-size: 1.7rem; margin: .5em 0;'>${teamOne.playerOne} & ${teamOne.playerTwo}</p><input class='hsPlayer' type='checkbox'/></div><div style='background-color: #1aa864; padding: .2em 0' margin: .2em 0><p style='font-size: 1.7rem; margin: .5em 0;'>${teamTwo.playerOne} & ${teamTwo.playerTwo}</p><input class='hsPlayer' type='checkbox'/></div><div style='background-color: #992b2e; padding: .2em 0' margin: .2em 0><p style='font-size: 1.7rem; margin: .5em 0;'>${teamThree.playerOne} & ${teamThree.playerTwo}</p><input class='hsPlayer' type='checkbox'/></div>`,
     text: ``,
     showCancelButton: true,
-    confirmButtonText: "Nyt Spil",
+    confirmButtonText: "Start Spil",
     cancelButtonText: "Tilbage",
     reverseButtons: true,
   })
   .then((result) => {
     if (result.isConfirmed) {
-      location.reload();
+      startSpiller();
+      Swal.fire({
+        position: "middle",
+        icon: "success",
+        title: `Spillet indlæses`,
+        /* <h1 style='margin-bottom: .8em; font-size: 2.3rem;'>${
+          DartGame.teamNames()[nx].value
+        }'s tur</h1><br><b style='font-size: 4rem;'>${player}</b><br> skal skyde <h2 class='timer' style='margin-top: .2em;'>om : ${sec}</h2>  */
+        showConfirmButton: false,
+        timer: 2000,
+      });
+      /* location.reload(); */
       console.log("nyt spil");
       return;
     }
@@ -383,10 +401,14 @@ highscorePlayers
     console.log("anulleret");
   });
 
+/* -------------------------------------------------------------------------- */
+/*                         Tilføj spillere click func                         */
+/* -------------------------------------------------------------------------- */
 let hsP = document.querySelectorAll(".hsPlayer");
+let hsRound = 0;
 hsP.forEach((player, index) => {
   player.addEventListener("click", () => {
-    let hsRound = 1;
+    let startPoint = [501, 501, 501];
     if (player.checked) {
       const pTable = document.createElement("div");
       pTable.className = "pTable";
@@ -394,11 +416,11 @@ hsP.forEach((player, index) => {
       if (index === 0) {
         console.log(`${teamOne.playerOne} & ${teamOne.playerTwo}`);
         player.style.pointerEvents = "none";
-        pTable.classList.add("hsActive");
-        pTable.innerHTML = `<table border="black">
+
+        pTable.innerHTML = `<table class='playerTable' border="black">
         <thead>
             <tr>
-                <th colspan="6">${teamOne.playerOne} & ${teamOne.playerTwo}</th>
+                <th class='playerHsName' colspan="6">${teamOne.playerOne} & ${teamOne.playerTwo}</th>
             </tr>
             <tr>
                 <th>Runde 1</th>
@@ -408,10 +430,10 @@ hsP.forEach((player, index) => {
             </tr>
             <tr>
               
-              <th class='round'>${hsRound}</th>
-              <th>0</th>
-              <th>0</th>
-              <th>0</th>
+              <th class='roundHs'></th>
+              <th class='roundHs'></th>
+              <th class='roundHs'></th>
+              <th class='hsStartPoint'>${startPoint[0]}</th>
           </tr>
         </thead>
         </table>`;
@@ -419,10 +441,11 @@ hsP.forEach((player, index) => {
       } else if (index === 1) {
         console.log(`${teamTwo.playerOne} & ${teamTwo.playerTwo}`);
         player.style.pointerEvents = "none";
-        pTable.innerHTML = `<table border="black">
+
+        pTable.innerHTML = `<table class='playerTable' border="black">
         <thead>
             <tr>
-                <th colspan="6">${teamTwo.playerOne} & ${teamTwo.playerTwo}</th>
+                <th class='playerHsName' colspan="6">${teamTwo.playerOne} & ${teamTwo.playerTwo}</th>
             </tr>
             <tr>
                 <th>Runde 1</th>
@@ -432,10 +455,10 @@ hsP.forEach((player, index) => {
             </tr>
             <tr>
               
-              <th class='round'>${hsRound}</th>
-              <th>0</th>
-              <th>0</th>
-              <th>0</th>
+              <th class='roundHs'></th>
+              <th class='roundHs'></th>
+              <th class='roundHs'></th>
+              <th class='hsStartPoint'>${startPoint[1]}</th>
           </tr>
         </thead>
         </table>`;
@@ -443,10 +466,10 @@ hsP.forEach((player, index) => {
       }
       console.log(`${teamThree.playerOne} & ${teamThree.playerTwo}`);
       player.style.pointerEvents = "none";
-      pTable.innerHTML = `<table border="black">
+      pTable.innerHTML = `<table class='playerTable' border="black">
         <thead>
             <tr>
-                <th colspan="6">${teamThree.playerOne} & ${teamThree.playerTwo}</th>
+                <th class='playerHsName' colspan="6">${teamThree.playerOne} & ${teamThree.playerTwo}</th>
             </tr>
             <tr>
                 <th>Runde 1</th>
@@ -456,20 +479,265 @@ hsP.forEach((player, index) => {
             </tr>
             <tr>
               
-              <th class='round'>${hsRound}</th>
-              <th>0</th>
-              <th>0</th>
-              <th>0</th>
+              <th class='roundHs'></th>
+              <th class='roundHs'></th>
+              <th class='roundHs'></th>
+              <th class='hsStartPoint'>${startPoint[2]}</th>
           </tr>
         </thead>
         </table>`;
     }
   });
 });
-console.log(hsP.length);
-document.querySelector(".highScore").addEventListener("click", () => {
-  document.querySelector(".board").classList.add("boardShow");
-});
+/* ------------------------- Definerer startspiller ------------------------- */
+/* player + pTable1 */
+/* let hsP = document.querySelectorAll(".hsPlayer"); */
+
+let pt = 0;
+let ct = 0;
+
+function startSpiller() {
+  let pT = document.querySelectorAll(".playerTable");
+  pT[pt].classList.add("hsActive");
+  const startHs = document.createElement("div");
+  startHs.classList.add("startHs");
+  document.querySelector(".table").appendChild(startHs);
+  startHs.innerHTML = `<button class='startHsBut'>Start Spil</button>`;
+  document.querySelector(".startHsBut").addEventListener("click", () => {
+    startHsGame();
+  });
+}
+function nextSpiller() {
+  let sec = 3;
+  let timer = setInterval(function () {
+    document.querySelector(".timer").innerHTML = `om : ${sec}`;
+    if (sec > 0) {
+      sec--;
+    } else {
+      sec = 3;
+      clearInterval(timer);
+    }
+  }, 750);
+  const cT = document.querySelectorAll(".playerHsName");
+  const pT = document.querySelectorAll(".playerTable");
+  pT[pt].classList.remove("hsActive");
+  pt++;
+  ct++;
+  if (pt === pT.length) {
+    pt = 0;
+    ct = 0;
+    hsRound++;
+  }
+  pT[pt].classList.add("hsActive");
+  console.log(cT[pt].innerHTML);
+
+  /* console.log(hsRound)
+  if (hsRound === pT.length) {
+    hsRound = 0;
+  } */
+  /*  if (hsRound % 2 == 0) {
+    if (ct === 0) {
+      player = teamOne.playerTwo;
+    } else if (ct === 1) {
+      player = teamTwo.playerTwo;
+    } else if (ct === 2) {
+      player = teamThree.playerTwo;
+    }
+  } else {
+    if (ct === 0) {
+      player = teamOne.playerOne;
+    } else if (ct === 1) {
+      player = teamTwo.playerOne;
+    } else if (ct === 2) {
+      player = teamThree.playerOne;
+    }
+  } */
+
+  /*  if(cT[0].innerHTML === 'Morten &amp; Torben' && cT[1].innerHTML === 'Lars &amp; Razzer'){
+    let hsPlayers = [
+      teamOne.playerOne,
+      teamOne.playerTwo,
+      teamTwo.playerOne,
+      teamTwo.playerTwo,
+      teamThree.playerOne,
+      teamThree.playerTwo,
+    ];
+  }else if(cT[1].innerHTML === 'Morten &amp; Torben' && cT[1].innerHTML === 'Jesper &amp; Jacob'){} */
+  Swal.fire({
+    position: "middle",
+    icon: "success",
+    title: `<h1 style='margin-bottom: .8em; font-size: 2.3rem;'>${cT[pt].innerHTML}'s tur</h1><br><b style='font-size: 4rem;'>${cT[pt].innerHTML}</b><br> skal skyde <h2 class='timer' style='margin-top: .2em;'>om : ${sec}</h2>`,
+    /* <h1 style='margin-bottom: .8em; font-size: 2.3rem;'>${
+      DartGame.teamNames()[nx].value
+    }'s tur</h1><br><b style='font-size: 4rem;'>${player}</b><br> skal skyde <h2 class='timer' style='margin-top: .2em;'>om : ${sec}</h2>  */
+    showConfirmButton: false,
+    timer: 3000,
+  });
+}
+
+function startHsGame() {
+  const cT = document.querySelectorAll(".playerHsName");
+  let sec = 3;
+  let timer = setInterval(function () {
+    document.querySelector(".timer").innerHTML = `om : ${sec}`;
+    if (sec > 0) {
+      sec--;
+    } else {
+      sec = 3;
+      clearInterval(timer);
+    }
+  }, 750);
+  Swal.fire({
+    position: "middle",
+    icon: "success",
+    title: `<h1 style='margin-bottom: .8em; font-size: 2.3rem;'>${cT[pt].innerHTML}'s tur</h1><br><b style='font-size: 4rem;'>${cT[pt].innerHTML}</b><br> skal skyde <h2 class='timer' style='margin-top: .2em;'>om : ${sec}</h2>`,
+    /* <h1 style='margin-bottom: .8em; font-size: 2.3rem;'>${
+      DartGame.teamNames()[nx].value
+    }'s tur</h1><br><b style='font-size: 4rem;'>${player}</b><br> skal skyde <h2 class='timer' style='margin-top: .2em;'>om : ${sec}</h2>  */
+    showConfirmButton: false,
+    timer: 3000,
+  });
+  const nextHs = document.createElement("div");
+  nextHs.classList.add("nextHs");
+  document.querySelector(".table").appendChild(nextHs);
+  nextHs.innerHTML = `<button class='nextHsBut'>Næste spiller</button>`;
+  document.querySelector(".startHs").remove();
+  document.querySelector(".nextHsBut").addEventListener("click", () => {
+    nextSpiller();
+  });
+}
+
+class DartField {
+  constructor(one, two, three, sp, dp, tp) {
+    this.one = one;
+    this.two = two;
+    this.three = three;
+    this.sp = sp;
+    this.dp = dp;
+    this.tp = tp;
+  }
+  dartBoard() {
+    let field = [
+      document.querySelector(`${this.one}`),
+      document.querySelector(`${this.two}`),
+      document.querySelector(`${this.three}`),
+    ];
+    field[0].addEventListener("click", () => {
+      collectPoints.push(this.sp);
+      console.log(collectPoints);
+      addHsPoint();
+    });
+    field[1].addEventListener("click", () => {
+      collectPoints.push(this.dp);
+      console.log(collectPoints);
+      addHsPoint();
+    });
+    field[2].addEventListener("click", () => {
+      collectPoints.push(this.tp);
+      console.log(collectPoints);
+      addHsPoint();
+    });
+  }
+}
+const dartFields = [
+  (One = new DartField("#s1", "#d1", "#t1", 1, 2, 3)),
+  (Two = new DartField("#s2", "#d2", "#t2", 2, 4, 6)),
+  (Three = new DartField("#s3", "#d3", "#t3", 6, 6, 9)),
+  (Four = new DartField("#s4", "#d4", "#t4", 4, 8, 12)),
+  (Five = new DartField("#s5", "#d5", "#t5", 5, 10, 15)),
+  (Six = new DartField("#s6", "#d6", "#t6", 6, 12, 18)),
+  (Seven = new DartField("#s7", "#d7", "#t7", 7, 14, 21)),
+  (Eight = new DartField("#s8", "#d8", "#t8", 8, 16, 24)),
+  (Nine = new DartField("#s9", "#d9", "#t9", 9, 18, 27)),
+  (Ten = new DartField("#s10", "#d10", "#t10", 10, 20, 30)),
+  (Elleven = new DartField("#s11", "#d11", "#t11", 11, 22, 33)),
+  (Twelve = new DartField("#s12", "#d12", "#t12", 12, 24, 36)),
+  (thirT = new DartField("#s13", "#d13", "#t13", 13, 26, 39)),
+  (FourT = new DartField("#s14", "#d14", "#t14", 14, 28, 42)),
+  (FiveT = new DartField("#s15", "#d15", "#t15", 15, 30, 45)),
+  (SixT = new DartField("#s16", "#d16", "#t16", 16, 32, 48)),
+  (SevenT = new DartField("#s17", "#d17", "#t17", 17, 34, 51)),
+  (EightT = new DartField("#s18", "#d18", "#t18", 18, 36, 54)),
+  (NittenT = new DartField("#s19", "#d19", "#t19", 19, 38, 57)),
+  (Twenty = new DartField("#s20", "#d20", "#t20", 20, 40, 60)),
+];
+let collectPoints = [];
+fields = () => {
+  for (let i = 0; i < dartFields.length; i++) {
+    dartFields[i].dartBoard();
+  }
+};
+fields();
+sumScore = () => {
+  const sum = [collectPoints[0], collectPoints[0], collectPoints[0]].reduce(
+    (partialSum, a) => partialSum + a,
+    0
+  );
+  console.log(sum); // 6
+};
+
+addHsPoint = () => {
+  let startPoint = [501, 501, 501];
+  let fullPoint = document.querySelectorAll(".hsStartPoint");
+  let roundPoint = document.querySelectorAll(".roundHs");
+  roundPoint[0].innerHTML = `${collectPoints[0]}`;
+  fullPoint[0].innerHTML = `${startPoint[0] - collectPoints[0]}`;
+  roundPoint[1].innerHTML = `${collectPoints[1]}`;
+  roundPoint[2].innerHTML = `${collectPoints[2]}`;
+};
+
+/* One = () => {
+one = 's1';
+two = 'd1';
+three = 't1';
+sp = 1;
+dp = 2;
+tp = 3;
+dartBoard(one, two, three, sp, dp, tp )
+};
+Two = () => {
+  one = 's2';
+  two = 'd2';
+  three = 't2';
+  sp = 2;
+  dp = 4;
+  tp = 6;
+  dartBoard(one, two, three, sp, dp, tp )
+  };
+  Three = () => {
+    one = 's3';
+    two = 'd3';
+    three = 't4';
+    sp = 3;
+    dp = 6;
+    tp = 9;
+    dartBoard(one, two, three, sp, dp, tp )
+    };
+    Four = () => {
+      one = 's4';
+      two = 'd4';
+      three = 't4';
+      sp = 1;
+      dp = 2;
+      tp = 3;
+      dartBoard(one, two, three, sp, dp, tp )
+      };
+      One = () => {
+        one = 's1';
+        two = 'd1';
+        three = 't1';
+        sp = 1;
+        dp = 2;
+        tp = 3;
+        dartBoard(one, two, three, sp, dp, tp )
+        };
+
+
+callFields = () => {
+  One()
+}
+callFields()
+ */
 
 /* const checkBoxes = document.createElement("span");
 checkBoxes.className = "check";
